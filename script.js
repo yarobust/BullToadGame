dbc1aa1eedda52866db53bcd8229fbb2a898ec0f
Yaroslav <yaroslav6289.42@gmail.com>
2023-03-26 16:02:00 +0300

    defining of main classes

--- script.js
+++ script.js
@@ -3,4 +3,30 @@ window.addEventListener('load', function(){
   const ctx = canvas.getContext('2d');
   canvas.width = 1200;
   canvas.height = 720;
+
+  class Player {
+    constructor(game){
+      this.game = game;
+
+    }
+  }
+  class Game {
+    //passing reference of main canvas
+    constructor(canvas){
+      this.canvas = canvas;
+      this.width = this.canvas.width;
+      this.height = this.canvas.height;
+      this.player = new Player(this);
+    }
+  }
+
+
+  //creaet instance of a game
+  const game = new Game(canvas);
+  console.log(game);
+
+  function animate(){
+
+  }
+
 });
