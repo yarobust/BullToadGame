2ae39cb6ea0d171c31d2554494b314d3dcb98d59
Yaroslav <yaroslav6289.42@gmail.com>
2023-03-26 18:41:02 +0300

    Drawing the player

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 window.addEventListener('load', function(){
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
+  //set up inital dimensions
   canvas.width = 1200;
   canvas.height = 720;
+
+  ctx.fillStyle = 'white';
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = 'white';
 
   class Player {
     constructor(game){
       this.game = game;
-
+      //hitbox
+      this.collisionX = this.game.width * 0.5;
+      this.collisionY = this.game.height * 0.5;
+      this.collisionRadius = 30;
+    }
+    draw(context){
+      context.beginPath();
+      context.arc(this.collisionX,this.collisionY, this.collisionRadius, 0,Math.PI * 2);
+      
+      context.save(); //saves the entire state onto a stack
+      context.globalAlpha = 0.5;
+      context.fill();
+      context.restore(); //restore most recently saved state
+      context.stroke();
     }
   }
   class Game {
@@ -18,11 +36,15 @@ window.addEventListener('load', function(){
       this.height = this.canvas.height;
       this.player = new Player(this);
     }
+    render(context){
+      this.player.draw(context);
+    }
   }
 
 
   //creaet instance of a game
   const game = new Game(canvas);
+  game.render(ctx);
   console.log(game);
 
   function animate(){
